18cfe28a7bddd004b84f1854f47e7af25d242a6e
zhukovka <helen.zhukova@gmail.com>
2016-11-07 13:53:36 +0200

    ng initial

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by lenka on 11/6/16.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'ng2-app',
+    template: '<h1>My First Angular App</h1>'
+})
+export class AppComponent { }
